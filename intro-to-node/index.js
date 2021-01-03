e61a984fce45dc2ab5536cde9837a87bf00b3a4a
Vipul Chauhan <viratdev07@gmail.com>
2021-01-03 23:34:36 +0530

    Adding get and post method and also send a req and res using server

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,5 +1,7 @@
 var superheroes = require("superheroes");
-
+var supervillains = require("supervillains");
 // String mySuperHeroName = superheroes.all();
 
 console.log(superheroes.random());
+
+console.log(supervillains.random());
