66d3afd9a9e48b4658117dab29be1ae6e0842ae4
Vipul Chauhan <viratdev07@gmail.com>
2021-01-03 22:02:19 +0530

    Adding route to the website

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+var bodyParser = require('body-parser');
 
 const app = express();
 
-
 const port = 3000;
+
+app.get("/", function(req, res) {
+  res.send("<h1>Hello World!</h1>");
+})
+
+
+app.get("/contact", function(req, res) {
+    res.send("Contact me at vipul@gmail.com");
+})
+
+
+app.get("/about-me", function(req, res) {
+    res.send("Hello, My name is Vipul Chauhan and I'm an Software Engineer at Anubudh EdTech!!");
+})
+
 
 
 app.listen(port, function () {
-    console.log("Server is running on 3000");
+    console.log(`Server is running on ${port}`);
 });
