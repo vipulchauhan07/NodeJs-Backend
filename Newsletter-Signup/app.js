a0d5b381ce1db57825e0f18a12cfcc69105c0a2e
Vipul Chauhan <viratdev07@gmail.com>
2021-01-14 08:38:13 +0530

    newsletter sign up

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const { static } = require('express');
 
 const app = express();
+
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res) {
-    res.send("Hello Your app is up and running.");
+    res.sendFile(__dirname + '/signup.html');
 });
 
+app.post('/', function (req, res) {
+    let fName = req.body.fName;
+    let lName = req.body.lName;
+    let email = req.body.email;
+    console.log(req.body);
+});
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000/");
-})
+});
+
+
+
+// 22037c09ae81f9e92c863fba9841cd20-us7 Api Key
